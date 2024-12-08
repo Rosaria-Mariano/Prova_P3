985379048eae01326c17b946a43ab1bd67006dfb
Rosaria Mariano <marianorosaria1957@gmail.com>
2024-12-08 20:43:57 -0300

    instalação templates

--- Projeto_P3/setup/urls.py
+++ Projeto_P3/setup/urls.py
@@ -19,4 +19,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TodoListView.as_view(), name='todo_list'),
+    path('create/', TodoCreateView.as_view(), name='todo_create'),
+    path('update/<int:pk>/', TodoUpdateView.as_view(), name='todo_update'),
+    path('delete/<int:pk>/', TodoDeleteView.as_view(), name='todo_delete')
 ]
--- Projeto_P3/todos/views.py
+++ Projeto_P3/todos/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from models import Todo
+from django.shortcuts import redirect
+from datetime import date  
 
-# Create your views here.
+
+class TodoListView(ListView):     
+ model = Todo 
+
+class TodoCreateView(CreateView):
+ model = Todo
+ fields = ['title','deadline']
+ success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+ model = Todo
+ fields = ['title', 'deadline']
+ success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(DeleteView):
+ model = Todo
+ success_url = reverse_lazy('todo_list')
+ 
